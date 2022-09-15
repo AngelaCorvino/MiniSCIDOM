e4f12891e57bb0d45b41058accf329e5c585ef94
angela.corvino@curie.fr <iosoprogrammare2022>
2022-09-15 16:42:04 +0200

    folder name changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-package_name = 'programs'
+package_name = 'reconstructionalgorithm'
 package_root = os.path.abspath('../../')
 sys.path.insert(0, package_root)
 sys.path.insert(0, os.path.join(package_root, package_name))
